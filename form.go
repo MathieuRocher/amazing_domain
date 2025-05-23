b71e03734882ac9fc30e9c002c44d9b2302da3ba
SwaiiZ <le.bail.bastienlb@gmail.com>
2025-05-23 10:29:45 +0200

    [FIX] Course assignment on form

--- form.go
+++ form.go
@@ -1,8 +1,9 @@
 package domain
 
 type Form struct {
-	ID            uint
-	MotherId      *uint
-	MotherForm    *Form
-	FormQuestions []FormQuestion
+	ID                 uint
+	MotherId           *uint
+	MotherForm         *Form
+	CourseAssignmentId *uint
+	FormQuestions      []FormQuestion
 }
